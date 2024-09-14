862182e5596930533d7f51196de0bc3c89d08440
phoenix-pnx <phoenix0pnx@gmail.com>
2024-09-15 02:41:13 +0600

    Update Phoenix.sol

--- Phoenix.sol
+++ Phoenix.sol
@@ -1,1 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+/// @custom:security-contact phoenix0pnx@gmail.com
+contract Phoenix is ERC20 {
+    constructor(address _addr) ERC20("Phoenix", "PNX") {
+        _mint(_addr, 1000 * 10 ** decimals());
+    }
+}
